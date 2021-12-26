7b20f5173fb3bc33f09d86dfc2bbe37e8686ca5b
bsun1220 <65567539+bsun1220@users.noreply.github.com>
2021-12-26 17:51:53 -0500

    Add files via upload

---
+++ LinkedLists/Add_Two_Numbers.java
@@ -0,0 +1,46 @@
+public class Add_Two_Numbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode ret = new ListNode(0, null);
+        ListNode start = ret;
+
+        int sum = 0;
+        int left = 0;
+        int right = 0;
+        boolean isGreat = false;
+        while(l1 != null || l2 != null)
+        {
+            left = l1 == null ? 0 : l1.val;
+            right = l2 == null ? 0 : l2.val;
+            sum = left + right;
+
+            if(isGreat)
+            {
+                sum++;
+                isGreat = false;
+            }
+            if(sum >= 10)
+            {
+                sum = sum % 10;
+                isGreat = true;
+            }
+
+            ret.next = new ListNode(sum, null);
+            ret = ret.next;
+
+            if(l1 != null)
+            {
+                l1 = l1.next;
+            }
+            if(l2 != null)
+            {
+                l2 = l2.next;
+            }
+        }
+        if (isGreat)
+        {
+            ret.next = new ListNode(1, null);
+        }
+
+        return start.next;
+    }
+}
