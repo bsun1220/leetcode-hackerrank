d4ee205f45ec1e0afe91859c583f214dcc848532
bsun1220 <65567539+bsun1220@users.noreply.github.com>
2022-01-15 21:35:19 -0500

    Add files via upload

---
+++ Trees-Graphs/BinaryTreeRightSide.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class BinaryTreeRightSide {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null){return list;}
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode curr = q.remove();
+                if(i == size - 1){
+                    list.add(curr.val);
+                }
+                if(curr.left != null){
+                    q.add(curr.left);
+                }
+                if(curr.right != null){
+                    q.add(curr.right);
+                }
+            }
+        }
+
+        return list;
+    }
+}
