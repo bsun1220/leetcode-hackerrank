817ea0210b193bc83e424567a2ac982e6d6c63c2
bsun1220 <65567539+bsun1220@users.noreply.github.com>
2022-05-22 15:46:46 -0400

    Add files via upload

---
+++ Recursion-DP/Tribonacci.java
@@ -0,0 +1,21 @@
+public class Tribonacci {
+    public int tribonacci(int n) {
+        if(n == 0){
+            return 0;
+        }
+
+        if(n == 1 || n == 2){
+            return 1;
+        }
+
+
+        int [] cache = new int [n + 1];
+        cache[0] = 0;
+        cache[1] = 1;
+        cache[2] = 1;
+        for(int i = 3; i < n + 1; i++){
+            cache[i] = cache[i - 3] + cache[i - 2] + cache[i-1];
+        }
+        return cache[n];
+    }
+}
