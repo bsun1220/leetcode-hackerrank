8f6bc35eb6acc5c736078d83142afb898c02e712
Benny Sun <65567539+bsun1220@users.noreply.github.com>
2022-06-15 10:25:50 -0400

    Add files via upload

---
+++ Greedy/PlaceFlowers.java
@@ -0,0 +1,14 @@
+public class PlaceFlowers {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for(int i = 0; i < flowerbed.length; i++){
+
+            int prev = i == 0 ? 0 : flowerbed[i - 1];
+            int next = i == flowerbed.length - 1 ? 0 : flowerbed[i + 1];
+            if(next == 0 && prev == 0 && flowerbed[i] == 0){
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+        return n <= 0;
+    }
+}
